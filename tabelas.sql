d5e2763663f839eb902089319c828992aa6af983
GGois <gbgois199@gmail.com>
2025-06-02 22:36:16 -0300

    Atualização de tabela para funcionamento de inserts sem id

--- tabelas.sql
+++ tabelas.sql
@@ -1,8 +1,9 @@
 CREATE UNLOGGED TABLE clientes_em_memoria (
-  id INT PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   nome TEXT,
   saldo NUMERIC
 );
+
 
 CREATE TABLE log (
   log_id SERIAL PRIMARY KEY,
@@ -11,6 +12,7 @@ CREATE TABLE log (
   nome TEXT,
   saldo NUMERIC
 );
+
 
 CREATE OR REPLACE FUNCTION trigger_log()
 RETURNS TRIGGER AS $$
@@ -44,38 +46,5 @@ CREATE TRIGGER trg_clientes_em_memoria_after_delete
 AFTER DELETE ON clientes_em_memoria
 FOR EACH ROW EXECUTE FUNCTION trigger_log();
 
-BEGIN;
-INSERT INTO clientes_em_memoria (id, nome, saldo) VALUES (1, 'Cliente 1', 100.00);
-UPDATE clientes_em_memoria SET saldo = saldo + 50 WHERE id = 1;
-END;
 
-BEGIN;
-INSERT INTO clientes_em_memoria (id, nome, saldo) VALUES (2, 'Cliente 2', 200.00);
-UPDATE clientes_em_memoria SET saldo = saldo + 50 WHERE id = 2;
-END;
 
-BEGIN;
-INSERT INTO clientes_em_memoria (id, nome, saldo) VALUES (3, 'Cliente 3', 300.00);
-UPDATE clientes_em_memoria SET saldo = saldo + 50 WHERE id = 2;
-END;
-
-BEGIN;
-INSERT INTO clientes_em_memoria (id, nome, saldo) VALUES (4, 'Cliente 4', 400.00);
-UPDATE clientes_em_memoria SET saldo = saldo + 50 WHERE id = 3;
-END;
-
-BEGIN;
-INSERT INTO clientes_em_memoria (id, nome, saldo) VALUES (5, 'Cliente 5', 500.00);
-END;
-
-BEGIN;
-UPDATE clientes_em_memoria SET saldo = 550.00 WHERE id = 5;
-END;
-
-BEGIN;
-DELETE FROM clientes_em_memoria WHERE id = 5;
-END;
-
-BEGIN;
-INSERT INTO clientes_em_memoria (id, nome, saldo) VALUES (6, 'Cliente 6', 600.00);
-END;
